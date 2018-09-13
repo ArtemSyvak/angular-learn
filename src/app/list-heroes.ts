09bd12c2db4f878ce69f5e719881adcedd3bf39c
artemsyvak <artsyvak@gmail.com>
2018-09-13 18:50:01 +0300

    created angular web-app for learning a framework

---
+++ src/app/list-heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Billy Elgeron'},
+  { id: 2, name: 'Storm Spirit'},
+  { id: 3, name: 'Monkey King'},
+  { id: 4, name: 'Earth Spirit'},
+  { id: 5, name: 'Rick & Morty'},
+  { id: 6, name: 'Ember Spirit'},
+  { id: 7, name: 'Forsaken Archer'}
+];
